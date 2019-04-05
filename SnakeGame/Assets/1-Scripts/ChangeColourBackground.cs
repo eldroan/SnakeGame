c0981938d449ec7cb485a0939ff75ba3727884ba
eldroan <leandroamarillo95@gmail.com>
2019-04-05 12:16:11 -0300

    Se agrega para que cambie de color el fondo al comer una fruta usando UnityEvent

---
+++ SnakeGame/Assets/1-Scripts/ChangeColourBackground.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColourBackground : MonoBehaviour
+{
+    [SerializeField] private Camera myCamera;
+    private List<Color> colorlist;
+    private int index;
+    void Awake()
+    {
+        myCamera = this.gameObject.GetComponent<Camera>();
+        
+        colorlist = new List<Color>();
+        colorlist.Add(Color.red);
+        colorlist.Add(Color.blue);
+        colorlist.Add(Color.cyan);
+        colorlist.Add(Color.green);
+        colorlist.Add(Color.grey);
+    }
+
+    // Update is called once per frame
+    public void ChangeBackGroundColour()
+    {
+        if (myCamera != null)
+        {
+            myCamera.backgroundColor = colorlist[index++ % colorlist.Count];
+        }
+    }
+}
