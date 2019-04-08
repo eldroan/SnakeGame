b6fadf4d71847f0b4e34a152f297f2a23d84618b
eldroan <leandroamarillo95@gmail.com>
2019-04-07 23:03:29 -0300

    Se agrega un contador de puntos utilizando UnityEvent

---
+++ SnakeGameV2/Assets/ScoreController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    [SerializeField] private Text scoreText;
+    [SerializeField] private int scorePoints;
+    private int _score;
+    
+    public void OnScorePoints()
+    {
+        _score += scorePoints;
+        scoreText.text = "Puntaje: " + _score;
+    }
+}
